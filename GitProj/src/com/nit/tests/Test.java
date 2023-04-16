59567b871a359010b51fa772bac14587608ce2db
Admin <Admin@DESKTOP-JRGD5JT>
2023-04-16 23:30:14 +0530

    dev1 modified test.java

--- GitProj/src/com/nit/tests/Test.java
+++ GitProj/src/com/nit/tests/Test.java
@@ -1,5 +1,10 @@
 package com.nit.tests;
 
+import com.nit.beens.Utility;
+
 public class Test {
-	
+	public static void main(String[] args) {
+		Utility u=new Utility();
+		System.out.println(u.getByDate());
+	}
 }
